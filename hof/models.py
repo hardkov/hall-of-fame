db1ab9640db559022f6410f4c6b21152f93e15c2
Mateusz Benecki <benecki.mateusz@gmail.com>
2020-04-17 00:57:51 +0200

    Fix models file and make an initial migration

--- hof/models.py
+++ hof/models.py
@@ -1,36 +1,9 @@
 import datetime
 from django.db import models
-import datetime as d
-from django.utils import datezone
 
 
-class Group(models.Model):
-    year = models.IntegerField(default=int(d.datetime.now().year))
-    day_of_the_week = models.CharField(max_length=10)
-    lecturer = models.CharField(max_length=30)
-
-    def __str__(self):
-        return f'{self.year}_{self.day_of_the_week}_{self.lecturer}'
-      
-
-class Score(models.Model):
-    task = models.ForeignKey(Task)
-    student = models.ForeignKey(Student)
-    acquired_blood_cells = models.IntegerField(default=0)
-    date = models.DateTimeField('due date')
-
-    def __str__(self):
-        return self.acquired_blood_cells
-
-      
-class Student(models.Model):
-    group = models.ForeignKey(Group)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    nickname = models.CharField(max_length=20, unique=True)
-
-    def __str__(self):
-        return self.nickname
+class TaskCollection(models.Model):
+    description = models.CharField(max_length=200)
 
 
 class Task(models.Model):
@@ -39,5 +12,35 @@ class Task(models.Model):
     description = models.CharField(max_length=200)
 
 
-class TaskCollection(models.Model):
-    description = models.CharField(max_length=200)
+class Group(models.Model):
+    year = models.IntegerField(default=int(datetime.datetime.now().year))
+    day_of_the_week = models.CharField(max_length=10)
+    lecturer = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.year}_{self.day_of_the_week}_{self.lecturer}'
+
+
+class Student(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.nickname
+
+
+class Score(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    acquired_blood_cells = models.IntegerField(default=0)
+    date = models.DateTimeField('due date')
+
+    def __str__(self):
+        return self.acquired_blood_cells
+
+
+
+
+
