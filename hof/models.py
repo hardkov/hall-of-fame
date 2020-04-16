5bd3d4599e59a10e4489dd92485ddcb64bda987c
dkawalecc <dkawalec08@gmail.com>
2020-04-17 00:34:19 +0200

    score model

--- hof/models.py
+++ hof/models.py
@@ -1,3 +1,15 @@
+import datetime
 from django.db import models
+from django.utils import datezone
+
 
 # Create your models here.
+
+class Score(models.Model):
+    task = models.ForeignKey(Task)
+    student = models.ForeignKey(Student)
+    acquired_blood_cells = models.IntegerField(default=0)
+    date = models.DateTimeField('due date')
+
+    def __str__(self):
+        return self.acquired_blood_cells
