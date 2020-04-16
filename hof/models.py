0f8404e9b60439ba5df15fce780d786774dce8ed
hardkov <krzysztofH150@interia.pl>
2020-04-17 00:33:00 +0200

    Group class added

--- hof/models.py
+++ hof/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime as d
 
-# Create your models here.
+
+class Group(models.Model):
+    year = models.IntegerField(default=int(d.datetime.now().year))
+    day_of_the_week = models.CharField(max_length=10)
+    lecturer = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.year}_{self.day_of_the_week}_{self.lecturer}'
