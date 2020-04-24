97224714fccc04c83f195d53b6f6074810f311ca
Mateusz Benecki <benecki.mateusz@gmail.com>
2020-04-24 19:07:13 +0200

    Register models in django admin

--- hof/admin.py
+++ hof/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+admin.site.register(TaskCollection)
+admin.site.register(Task)
+admin.site.register(Group)
+admin.site.register(Student)
+admin.site.register(Score)
