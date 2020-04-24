b0197f1277cd9db1f7bc24c3c19309399f005d33
Mateusz Benecki <benecki.mateusz@gmail.com>
2020-04-24 19:06:59 +0200

    Add basic template + sketch index.html

--- hof/views.py
+++ hof/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('works')
+    return render(request, 'hof/index.html')
 
 
 def test(request):
