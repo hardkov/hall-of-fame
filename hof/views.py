e84ec5da1ab6d2895632784890b891fbd134f588
dkawalecc <dkawalec08@gmail.com>
2020-05-28 12:36:48 +0200

    Groups front

--- hof/views.py
+++ hof/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import generic
 
-from .models import Group, Student, Score
+from .models import Group, Student
 
 
 # Create your views here.
